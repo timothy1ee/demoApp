9068278a63b59e0bfccb6d55c56d44dce6bbe597
Timothy Lee <tim@codepath.com>
2019-01-14 12:28:34 -0800

    Initial Commit

---
+++ demoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoApp
+//
+//  Created by Timothy Lee on 1/14/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
