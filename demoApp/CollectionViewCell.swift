41611b4eff8037f89ec88e84e236cdb1f4224502
Timothy Lee <tim@codepath.com>
2019-01-14 17:57:25 -0800

    Stuff

---
+++ demoApp/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  demoApp
+//
+//  Created by Timothy Lee on 1/14/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
